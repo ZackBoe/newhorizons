1b8f4f73a841efc2f7b5301de81a1cec3a27ab88
Zack Boehm <git@zackboehm.com>
2020-11-23 01:45:54 -0500

    Use netlify chromium plugin

--- src/_data/events.js
+++ src/_data/events.js
@@ -9,11 +9,11 @@ module.exports = async function() {
 
   // return [{emoji: '🌎',name: 'Earth Day?',date: '📣 Starts in 6 days 📅 Apr 22',outcome: '🤷‍♂️ No known recipes or rewards',link: 'https://www.notion.so/Earth-Day-51292eb73a364390a3bc0053aac27c01'},{emoji: '🎍',name: 'Young Spring Bamboo',date: '⏳ 45 days left 📅 Mar 01 → May 31',outcome: '🧾 9/10 Recipes 🛠 3 Crafted',link: 'https://www.notion.so/Young-Spring-Bamboo-a6f1fb1a244a4825afb2173e18168e88'}]
 
-  console.info('Launching headless browser')
+  console.info('Launching headless browser from ', process.env.CHROME_PATH)
   let browser = null
 
   try {
-    browser = await chromium.launch({ headless: true })
+    browser = await chromium.launch({ headless: true, executablePath: process.env.CHROME_PATH })
     const page = await browser.newPage()
     await page.goto('https://www.notion.so/7df84d49a6944e7b8eb2a9957a746575')
     await page.waitForSelector(selector)
@@ -29,6 +29,7 @@ module.exports = async function() {
 
     if(typeof events === 'object' && events.length > 0) {
       events = events.map(event => {
+        console.log(event.text)
         text = event.text.split(/\n/)
         text.shift()
         console.log(text)
