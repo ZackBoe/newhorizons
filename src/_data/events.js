c7a58c714cbfed056e15e6d39f0ffb01780bd1cd
Zack Boehm <git@zackboehm.com>
2020-11-23 01:37:30 -0500

    Updating playwright

--- src/_data/events.js
+++ src/_data/events.js
@@ -1,7 +1,6 @@
 const dayjs = require('dayjs')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
-const playwright = require('playwright-aws-lambda');
-const chromium = require('playwright-chromium');
+const {chromium} = require('playwright-chromium');
 dayjs.extend(customParseFormat)
 
 const selector = '.notion-gallery-view [data-block-id="7df84d49-a694-4e7b-8eb2-a9957a746575"] .notion-collection-item'
@@ -10,21 +9,22 @@ module.exports = async function() {
 
   // return [{emoji: '🌎',name: 'Earth Day?',date: '📣 Starts in 6 days 📅 Apr 22',outcome: '🤷‍♂️ No known recipes or rewards',link: 'https://www.notion.so/Earth-Day-51292eb73a364390a3bc0053aac27c01'},{emoji: '🎍',name: 'Young Spring Bamboo',date: '⏳ 45 days left 📅 Mar 01 → May 31',outcome: '🧾 9/10 Recipes 🛠 3 Crafted',link: 'https://www.notion.so/Young-Spring-Bamboo-a6f1fb1a244a4825afb2173e18168e88'}]
 
-  console.info('Launching headless browser from ', chromium.chromium._executablePath)
+  console.info('Launching headless browser from ', process.env.CHROME_PATH)
   let browser = null
 
   try {
-    browser = await playwright.launchChromium({
+    browser = await chromium.launch({
       headless: true,
-      executablePath: chromium.chromium._executablePath
+      // executablePath: process.env.CHROME_PATH
     });
-    const context = await browser._defaultContext;
-    const page = await context.newPage()
+    // const context = await browser._defaultContext;
+    const page = await browser.newPage()
     await page.goto('https://www.notion.so/7df84d49a6944e7b8eb2a9957a746575')
     await page.waitForSelector(selector)
-    await page.waitFor(1000);
+    await page.waitForTimeout(1000);
     let events = await page.$$eval(selector, elements => [...elements].map((event) => {
       return {
+        emoji: event.querySelector('.notion-emoji').alt,
         text: event.innerText,
         href: event.firstChild.href
       }
@@ -36,10 +36,10 @@ module.exports = async function() {
         text = event.text.split(/\n/)
         text.shift()
         return {
-          emoji: text[0],
-          name: text[1],
-          date: text[2].split(`, ${dayjs().format('YYYY')}`).join(''),
-          outcome: text[3],
+          emoji: event.emoji,
+          name: text[0],
+          date: text[1].split(`, ${dayjs().format('YYYY')}`).join(''),
+          outcome: text[2],
           link: event.href
         }
       })
